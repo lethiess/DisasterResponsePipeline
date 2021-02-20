62e8c49d685faa2b34c5af1ccab47c7db2e67632
Leonard <lethiess@gmail.com>
2021-02-20 18:41:06 +0100

    Update template files for ETL pipeline and model

---
+++ data/process_data.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def load_data(messages_filepath, categories_filepath):
+    # read in file
+
+
+    # clean data
+
+
+    # load to database
+
+
+    # define features and label arrays
+
+    pass
+
+
+def clean_data(df):
+    pass
+
+
+def save_data(df, database_filename):
+    pass  
+
+
+
+
+def run_pipeline(data_file):
+    X, y = load_data(data_file)  # run ETL pipeline
+    model = build_model()  # build model pipeline
+    model = train(X, y, model)  # train model pipeline
+    export_model(model)  # save model
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
