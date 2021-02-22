c09d171dcd981252cea70b6fcafbb6675c129ae1
Leonard <lethiess@gmail.com>
2021-02-22 21:09:09 +0100

    Added basic ML pipeline functionality
    All basic functions are implemented. Now it is possible to
    load data from a DB, build a ML model pipeline and train and
    evaluate this model. This model can be saved as a pickle file.
    
    TODO:
    - more advanced ML pipeline
    - use Girdsearch
    - improove evaluation

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,64 +1,153 @@
 import sys
+from collections import defaultdict 
+import pickle
+
+import nltk
+import pandas as pd
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
 
 
 def load_data(database_filepath):
-    # read in file
+    '''
+    Load data from a datebase and split it into variables
+    for training a machine learning model. The data in the
+    database is the preprocessed data from the ETL pipeline.
 
+    Args:
+        database_filename: The filepath and name of the database.
 
-    # clean data
+    Returns:
+        X: This pandas series contrains all messages.
+        Y: This pandas series contrains the response to X.
+        categories: This list contains all categories of Y.
+    '''
+    # open database and read data to a pandas dataframe
+    engine = create_engine("sqlite:///"+database_filepath)
+    df = pd.read_sql("SELECT * FROM DisasterResponse", engine)
 
+    # split dataframe in predictor and outcome variables    
+    X = df["message"]   
+    Y = df.iloc[: , 4:]     
+    categories = Y.columns
 
-    # load to database
-
-
-    # define features and label arrays
-
-
-    pass
+    return X, Y, categories
 
 
 def tokenize(text):
-    pass
+    '''
+    Split the text of each disaster message into useful tokens.
+
+    Args:
+        text: The text of the disaster messages.
+
+    Returns:
+        A tokenized version of the original disaster messages.
+    '''
+    # use nltk to tokenize the message text
+    tokens = word_tokenize(text)
+
+    # lemmatize the tokens of each message 
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    # text processing and model pipeline
+    '''
+    Building a machine leanring model using 
+    sklearn's pipeline.
 
+    Args:
+        None
+    
+    Returns:
+        The pipeline of the model.
+    '''
+    # text processing and model pipeline
+    pipeline = Pipeline([
+        ("vect", CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ("clf", MultiOutputClassifier(RandomForestClassifier()))
+    ])
 
     # define parameters for GridSearchCV
-
-
     # create gridsearch object and return as final model pipeline
 
-
-    pass
-
-def train(X, y, model):
-    # train test split
-
-
-    # fit model
-
-
-    # output model test results
-
-
-    return model
-
-
-def export_model(model):
-    # Export model as a pickle file
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Predict the outcome of the model using the unknown test data.
+    The predictions will be evaluated with scikit-learn functions,
+    the focus is on f1-socre and accurary.    
+
+    Args:
+        model:  The model (pipeline) which should be evaluated.
+        X_test: Training data input (desaster messages).
+        Y_test: Training data output (disaster category)
+        category_names: Names of the disaster categories.
+
+    Returns:
+        None
+    '''
+    # predict 
+    Y_pred = pd.Dataframe(pipeline.predict(X_test))
+
+    # evaluate each predicted category
+    reports = {}
+    for category in category_names:
+        output = classification_report(Y_test[category], Y_pred_df[category], output_dict=True)
+        
+        reports[category] = {}
+        for out in output:
+            if out == "accuracy":
+                reports[category]["accuracy"] = output[category]
+            else:
+                reports[category]["f1_score_"+out] = output[category]["f1-score"]
+                reports[category]["precision_"+out] = output[category]["precision"]
+                reports[category]["recall_"+out] = output[category]["recall"]
+
+    # convert reports to pandas dataframe
+    df_reports = pd.DataFrame(reports).T
+    df_reports_mean = df_reports.mean()
+    print(df_reports_mean)
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+
+
+    Args:
+        model:
+        model_filepath:
+    
+    Returns:
+
+    '''
+    pickle.dump(model, open(filename, "wb"))
 
 
 def main():
+    # download the required nltk packages
+    nltk.download(["punkt", "wordnet"])
+
+    # check if the number of arguments are correct
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
@@ -74,8 +163,8 @@ def main():
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        #print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        #save_model(model, model_filepath)
 
         print('Trained model saved!')
 
