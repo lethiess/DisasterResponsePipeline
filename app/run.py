a7653b3b8cc83ff2e09342ab820a397379d081cb
Leonard <lethiess@gmail.com>
2021-02-27 18:17:41 +0100

    Update file path for the webapp

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +26,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+db_name = "DisasterResponse"
+engine = create_engine("sqlite:///../data/" + db_name + ".DB")
+df = pd.read_sql_table(db_name, engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model_name = "DisasterResponseModel"
+model = joblib.load("../models/" + model_name + ".pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
